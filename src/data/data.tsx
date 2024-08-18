442e1beb5d02ca77ed4ec3bc66c4216a69e0e12e
smox3y <smokeyboy181@gmail.com>
2024-08-18 18:28:06 -0500

    j

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -22,8 +22,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: "React Resume Template",
-  description: "Example site built with Tim Baker's react resume template",
+  title: "PANGEA",
+  description: "ACCELERATING HUMANITY AND MANKIND",
 };
 
 /**
