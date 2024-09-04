43e531b40b62ad4ce725eccfe09fac72ef471780
SM0X3Y <smokeyboy181@gmail.com>
2024-09-04 17:36:57 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,21 @@
 import dynamic from 'next/dynamic';
-import {FC, memo} from 'react';
+import { FC, memo } from 'react';
 
-import Page from '../components/Layout/Page';
-import Hero from '../components/Sections/Hero';
 import About from '../components/Sections/About';
-import {homePageMeta} from '../data/data';
+import Hero from '../components/Sections/Hero';
+import Page from '../components/Layout/Page';
+import { homePageMeta } from '../data/data';
 
 // eslint-disable-next-line react-memo/require-memo
-const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
+const Header = dynamic(() => import('../components/Sections/Header'), { ssr: false });
 
 const Home: FC = memo(() => {
-  const {title, description} = homePageMeta;
+  const { title, description } = homePageMeta;
   return (
     <Page description={description} title={title}>
       <Header />
       <About />
       <Hero />
-
     </Page>
   );
 });
