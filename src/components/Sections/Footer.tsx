e0eab098ffc3f2b7e0e1fd40f3e046df0eb9094e
smox3y <113198278+smox3y@users.noreply.github.com>
2024-09-06 23:13:43 -0500

    Update Footer.tsx

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -4,7 +4,6 @@ import {FC, memo} from 'react';
 import {SectionId} from '../../data/data';
 import Socials from '../Socials';
 
-const currentYear = new Date().getFullYear();
 
 const Footer: FC = memo(() => (
   <div className="relative bg-neutral-900 px-4 pb-6 pt-12 sm:px-8 sm:pb-8 sm:pt-14">
@@ -21,14 +20,12 @@ const Footer: FC = memo(() => (
       </div>
       <a
         className="-m-2 flex items-center gap-x-1 rounded-md p-2 ring-yellow focus:outline-none focus:ring-2"
-        href="https://reactresume.com">
+        href="https://iseeoneworld.com">
         <BoltIcon className="h-5 w-5 text-yellow" />
         <span>
-          Provided by <span className="text-white">React</span>
-          <span className="italic text-yellow">Resume</span>
+          Provided by <span className="text-white">Pangea</span>
         </span>
       </a>
-      <span className="text-sm text-neutral-700">© Copyright {currentYear} Tim Baker</span>
     </div>
   </div>
 ));
